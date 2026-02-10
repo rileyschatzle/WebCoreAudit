4e1b0c0772c0e5a65b893a86fd01c82fae6c2953
Riley Schatzle <riley@rileyschatzle.com>
2026-02-10 00:13:01 +0000

    Disable Suspense bailout error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Ignore build errors for pages with useSearchParams not wrapped in Suspense
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
